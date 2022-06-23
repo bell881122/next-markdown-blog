3cb20a5013cbb0178580d549433bee917693bc09
Emi <emi_chang@lhwlfw.com>
2022-06-23 09:41:53 +0800

    調整主版樣式、header & footer

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,31 @@
 import Link from 'next/link';
 
 export default function Layout({ children }) {
+  const year = new Date().getFullYear();
+
   return (
     <div className='flex flex-col min-h-screen'>
-      <header className='bg-fuchsia-100 mb-8 py-4'>
+      <header className='border-b-2 mb-4 px-3 py-4'>
         <div className='container mx-auto flex justify-center'>
           <Link href='/'>
-            <a>🏡</a>
+            <a className='mx-auto text-3xl font-bold'>笑笑的程式人生</a>
           </Link>
-          <span className='mx-auto'>Welcome to my blog</span>{' '}
         </div>
       </header>
       <main className='container mx-auto flex-1'>{children}</main>
-      <footer className='bg-fuchsia-100 mt-8 py-4'>
+      <footer className='bg-gray-200 mt-8 py-4'>
         <div className='container mx-auto flex justify-center'>
-          &copy; 2022 DailyDevTips
+          <span>&copy; {year}
+            <Link href='https://github.com/bell881122'>
+              <a className='mx-1 text-gray-400'>Emi Chang</a>
+            </Link>
+            <span className='mx-3'>|</span>
+          </span>
+          <small className='mx-1 my-auto'>Theme by
+            <Link href='https://github.com/rebelchris/next-markdown-blog'>
+              <a className='mx-1 text-gray-400'>rebelchris</a>
+            </Link>
+          </small>
         </div>
       </footer>
     </div>
